437ddb1c4c835e69416bda387a08fde4a89211f4
AlexLaime <alaimes64@gmail.com>
2022-11-05 13:13:44 -0500

    ultimate

--- src/app/proyectos/proyecto/modal/view-proyecto/view-proyecto.component.ts
+++ src/app/proyectos/proyecto/modal/view-proyecto/view-proyecto.component.ts
@@ -2,7 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { EmpleadoService } from 'src/app/empleados/empleado/services/empleado.service';
 import { Proyecto } from 'src/app/entities/modulos/proyecto';
+import { LoginService } from 'src/app/services/security/login.service';
 import { ProyectoService } from '../../services/proyecto.service';
 import { ReporteService } from '../../services/reporte.service';
 
@@ -29,12 +31,20 @@ export class ViewProyectoComponent implements OnInit {
   public formSubmited = false;
   public totalReporte: number = 0;
 
+  public totalHorasTrabajadas = 0;
+  public totalPagoResponsable = 0;
 
+  // public subTotal = this.proyecto.presupuesto = this.totalHorasTrabajadas * this.totalPagoResponsable ;
+  typeUser
+
+  
   constructor(
     protected formService: ProyectoService,
+    protected  empleadoService: EmpleadoService,
     protected reporteService: ReporteService,
     protected activeModal: NgbActiveModal,
-    private snackBar: MatSnackBar) {
+    private snackBar: MatSnackBar,
+    protected loginService: LoginService) {
   }
 
   ngOnInit(): void {
@@ -42,12 +52,28 @@ export class ViewProyectoComponent implements OnInit {
       console.log(data['proyecto'][0])
       console.log("cargo editar")
       this.formService.fillForm = data['proyecto'][0]
-      this.formLoaded = true
+      this.formLoaded = true;
+
+
+      this.typeUser = this.loginService.getLogin()?.role
+
+
+      this.empleadoService.getById({ "_id": data['proyecto'][0].responsable}).subscribe(data => {
+        this.totalPagoResponsable = data['personal'][0]['pago'];
+        console.log('CANTIDAD DE PAGO EMPLEADO');
+        console.log(this.totalPagoResponsable) 
+      })
+
 
       this.reporteService.getByReportes(this.desde, { "proyectoID":  data['proyecto'][0]._id})
       .subscribe(({total, reporte}) => {
           console.log(reporte);
           this.totalReporte = total;
+
+          this.totalHorasTrabajadas = reporte.reduce((sum, value) => (typeof value.horas == "number" ? sum + value.horas : sum), 0);
+          console.log('CANTIDAD DE HORAS TRABAJDAS');
+          console.log(this.totalHorasTrabajadas);
+
           if(reporte.length !== 0) { 
             this.reporte = reporte;
             console.log(reporte);
