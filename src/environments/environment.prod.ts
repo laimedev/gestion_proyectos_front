dd7e5ae07d533ced7347e510880209d090182a72
AlexLaime <alaimes64@gmail.com>
2023-01-02 10:06:02 -0500

    deee

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  // baseUrl: 'https://gestionservices.herokuapp.com/',
-  // url: 'https://gestionservices.herokuapp.com/',
-  baseUrl: 'http://localhost:3000/',
-  url: 'http://localhost:3000/',
+  baseUrl: 'https://gestionservices.herokuapp.com/',
+  url: 'https://gestionservices.herokuapp.com/',
+  // baseUrl: 'http://localhost:3000/',
+  // url: 'http://localhost:3000/',
   appTitle: 'GESTION PRY.'
 };
